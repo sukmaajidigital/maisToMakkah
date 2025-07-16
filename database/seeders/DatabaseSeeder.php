89b3499091539dad46a1f480bbcd0de8413c7a43
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-07-16 15:36:41 +0700

    Add Admin model and update related factory and seeder
    Introduces a new Admin model for authentication. Renames UserFactory to AdminFactory and updates DatabaseSeeder to use Admin instead of User for seeding test data.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        Admin::factory()->create([
             'name' => 'testuser',
             'email' => 'test@example.com',
         ]);
