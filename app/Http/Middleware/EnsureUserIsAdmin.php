830ef4d528cc3a33691e2fa9240400fbe1094dc6
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-07-16 22:48:28 +0700

    MIDDLEWARE : Add admin authentication and middleware
    Introduced an EnsureUserIsAdmin middleware to restrict access to admin-only routes. Registered the middleware alias and added 'admin' guard and provider configuration in auth.php for handling admin authentication using the Admin model.

---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (Auth::guard('admin')->check()) {
+            return $next($request);
+        }
+
+        abort(403, 'AKSES DITOLAK. ANDA TIDAK MEMILIKI HAK UNTUK MENGAKSES HALAMAN INI.');
+    }
+}
