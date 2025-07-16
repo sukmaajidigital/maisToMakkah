d7d68ec14611ddf714874127443478d086746261
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-07-16 18:50:37 +0700

    Migration : fix migration for relational database

---
+++ database/migrations/2025_07_16_114846_add_collumn_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('parent_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('rank_id')->constrained('ranks');
+            $table->decimal('bonus_balance', 15, 2)->default(0);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Drop foreign keys first
+            $table->dropForeign(['parent_id']);
+            $table->dropForeign(['rank_id']);
+
+            // Then drop the columns
+            $table->dropColumn(['parent_id', 'rank_id', 'bonus_balance']);
+        });
+    }
+};
